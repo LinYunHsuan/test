4a9769d95690a1b65d730c9d2a26dd8e5c6d4f33
LinYunHsuan <42128428+LinYunHsuan@users.noreply.github.com>
2018-10-03 13:25:52 +0800

    Delete opntest.js

--- opntest.js
+++
@@ -1,8 +0,0 @@
-var opn = require('opn');
-var express = require('express');
-
-opn('http://www.google.com.tw').then(() => {
-                 console.log('open success');
-             }).catch(function(){
-                console.log('open fail');
-                 });
